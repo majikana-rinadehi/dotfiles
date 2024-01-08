eaf3609c20035efb18560fb5aa78828a34255e2f
NH <rudyrudy2103@gmail.com>
2024-01-08 15:07:03 +0900

    fix install.sh - Devcontainer互換性のため、配列構文使わない方法で書き直し

--- install.sh
+++ install.sh
@@ -2,21 +2,15 @@
 
 DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-#除外対象のファイル
-exclude_files=(".git" ".github" ".DS_Store")
-
 for dotfile in "${DOTFILES_DIR}"/.??*; do
 
     # ファイル名のみを取得（フルパスから）
     filename=$(basename "$dotfile")
 
-    # 配列内の各要素と比較
-    for exclude in "${exclude_files[@]}"; do
-        if [[ "$filename" == "$exclude" ]]; then
-            # 一致する場合は次のdotfileに進む
-            continue 2
-        fi
-    done
+    # 特定のファイルを除外
+    if [[ "$filename" == ".git" || "$filename" == ".github" || "$filename" == ".DS_Store" ]]; then
+        continue
+    fi
 
     ln -fnsv "$dotfile" "$HOME"
 done
